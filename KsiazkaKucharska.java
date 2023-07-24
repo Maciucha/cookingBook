1289cb508dc037befb2ff45aed49df16f1025015
Maciej Żok <maciucha@tlen.pl>
2023-07-25 00:13:54 +0200

    cookingBook

--- KsiazkaKucharska.java
+++ KsiazkaKucharska.java
@@ -1,165 +1,165 @@
-    package cookingBook;
+package cookingBook;
 
-    import java.io.*;
-    import java.util.ArrayList;
-    import javax.swing.JOptionPane;
-    import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.io.Serializable;
 
-    public class KsiazkaKucharska implements Serializable {
-        @Serial
-        private static final long serialVersionUID = 1L;
+public class KsiazkaKucharska implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-        private ArrayList<Przepis> przepisy;
+    private ArrayList<Przepis> przepisy;
 
-        public KsiazkaKucharska() {
-            przepisy = new ArrayList<>();
+    public KsiazkaKucharska() {
+        przepisy = new ArrayList<>();
+    }
+
+    public void dodajPrzepis() {
+        String nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu:");
+        ArrayList<Skladnik> skladniki = new ArrayList<>();
+        String nazwaSkladnika;
+        boolean wprowadzanieSkladnikow = true;
+        while (wprowadzanieSkladnikow) {
+            nazwaSkladnika = JOptionPane.showInputDialog("Podaj nazwe skladnika:");
+            String iloscSkladnikaString = JOptionPane.showInputDialog("Podaj ilosc skladnika:");
+            wprowadzanieSkladnikow = isWprowadzanieSkladnikow(skladniki, nazwaSkladnika, wprowadzanieSkladnikow, iloscSkladnikaString);
         }
+        String sposobWykonania = JOptionPane.showInputDialog("Podaj sposob wykonania przepisu:");
+        przepisy.add(new Przepis(nazwaPrzepisu, skladniki, sposobWykonania));
+        JOptionPane.showMessageDialog(null, "Przepis dodany do ksiazki kucharskiej.");
+    }
 
-        public void dodajPrzepis() {
-            String nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu:");
-            ArrayList<Skladnik> skladniki = new ArrayList<>();
-            String nazwaSkladnika;
-            boolean wprowadzanieSkladnikow = true;
-            while (wprowadzanieSkladnikow) {
-                nazwaSkladnika = JOptionPane.showInputDialog("Podaj nazwe skladnika:");
-                String iloscSkladnikaString = JOptionPane.showInputDialog("Podaj ilosc skladnika:");
-                wprowadzanieSkladnikow = isWprowadzanieSkladnikow(skladniki, nazwaSkladnika, wprowadzanieSkladnikow, iloscSkladnikaString);
-            }
-            String sposobWykonania = JOptionPane.showInputDialog("Podaj sposob wykonania przepisu:");
-            przepisy.add(new Przepis(nazwaPrzepisu, skladniki, sposobWykonania));
-            JOptionPane.showMessageDialog(null, "Przepis dodany do ksiazki kucharskiej.");
-        }
-
-        private boolean isWprowadzanieSkladnikow(ArrayList<Skladnik> skladniki, String nazwaSkladnika, boolean wprowadzanieSkladnikow, String iloscSkladnikaString) {
-            double iloscSkladnika;
-            iloscSkladnikaString = iloscSkladnikaString.replace(",", ".");
-            String jednostkaMiarySkladnika;
-            try {
-                iloscSkladnika = Double.parseDouble(iloscSkladnikaString);
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Nieprawidlowy format ilosci skladnika");
-                return wprowadzanieSkladnikow;
-            }
-            jednostkaMiarySkladnika = JOptionPane.showInputDialog("Podaj jednostke miary skladnika:");
-            skladniki.add(new Skladnik(nazwaSkladnika, iloscSkladnika, jednostkaMiarySkladnika));
-            int choice = JOptionPane.showConfirmDialog(null, "Czy chcesz dodac kolejny skladnik?", "Dodawanie skladnikow", JOptionPane.YES_NO_OPTION);
-            if (choice == JOptionPane.NO_OPTION) {
-                wprowadzanieSkladnikow = false;
-            }
+    private boolean isWprowadzanieSkladnikow(ArrayList<Skladnik> skladniki, String nazwaSkladnika, boolean wprowadzanieSkladnikow, String iloscSkladnikaString) {
+        double iloscSkladnika;
+        iloscSkladnikaString = iloscSkladnikaString.replace(",", ".");
+        String jednostkaMiarySkladnika;
+        try {
+            iloscSkladnika = Double.parseDouble(iloscSkladnikaString);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Nieprawidlowy format ilosci skladnika");
             return wprowadzanieSkladnikow;
         }
-
-        public void wyswietlListePrzepisow() {
-            StringBuilder listaPrzepisow = new StringBuilder();
-            for (Przepis przepis : przepisy) {
-                listaPrzepisow.append(przepis.getNazwa()).append("\n");
-            }
-            JOptionPane.showMessageDialog(null, listaPrzepisow.toString());
+        jednostkaMiarySkladnika = JOptionPane.showInputDialog("Podaj jednostke miary skladnika:");
+        skladniki.add(new Skladnik(nazwaSkladnika, iloscSkladnika, jednostkaMiarySkladnika));
+        int choice = JOptionPane.showConfirmDialog(null, "Czy chcesz dodac kolejny skladnik?", "Dodawanie skladnikow", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.NO_OPTION) {
+            wprowadzanieSkladnikow = false;
         }
+        return wprowadzanieSkladnikow;
+    }
 
-        public void wyswietlPrzepis(String nazwaPrzepisu) {
-            boolean przepisZnaleziony = false;
-            for (Przepis value : przepisy) {
-                if (value.getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
-                    StringBuilder skladniki = new StringBuilder();
-                    for (Skladnik skladnik : value.getSkladniki()) {
-                        skladniki.append(skladnik.getNazwa()).append(" - ").append(skladnik.getIlosc()).append(" ").append(skladnik.getJednostkaMiary()).append("\n");
+    public void wyswietlListePrzepisow() {
+        StringBuilder listaPrzepisow = new StringBuilder();
+        for (Przepis przepis : przepisy) {
+            listaPrzepisow.append(przepis.getNazwa()).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, listaPrzepisow.toString());
+    }
+
+    public void wyswietlPrzepis(String nazwaPrzepisu) {
+        boolean przepisZnaleziony = false;
+        for (Przepis value : przepisy) {
+            if (value.getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
+                StringBuilder skladniki = new StringBuilder();
+                for (Skladnik skladnik : value.getSkladniki()) {
+                    skladniki.append(skladnik.getNazwa()).append(" - ").append(skladnik.getIlosc()).append(" ").append(skladnik.getJednostkaMiary()).append("\n");
+                }
+                String sposobWykonania = value.getSposobWykonania();
+                String infoPrzepis = "Nazwa: " + value.getNazwa() + "\nSkladniki:\n" + skladniki + "Sposób wykonania:\n" + sposobWykonania;
+                JOptionPane.showMessageDialog(null, infoPrzepis);
+                przepisZnaleziony = true;
+                break;
+            }
+        }
+        if (!przepisZnaleziony) {
+            JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
+        }
+    }
+
+    public void edytujPrzepis(String nazwaPrzepisu) {
+        nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, który chcesz edytować:");
+        boolean przepisZnaleziony = false;
+        for (Przepis przepis : przepisy) {
+            if (przepis.getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
+                // Similar logic as in the dodajPrzepis() method to edit the recipe
+                // ...
+                przepisZnaleziony = true;
+                break;
+            }
+        }
+        if (!przepisZnaleziony) {
+            JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
+        }
+    }
+
+    public void usunPrzepis(String nazwaPrzepisu) {
+        nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, który chcesz usunąć:");
+        boolean przepisZnaleziony = false;
+        for (int i = 0; i < przepisy.size(); i++) {
+            if (przepisy.get(i).getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
+                przepisy.remove(i);
+                JOptionPane.showMessageDialog(null, "Przepis usunięty.");
+                przepisZnaleziony = true;
+                break;
+            }
+        }
+        if (!przepisZnaleziony) {
+            JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
+        }
+    }
+
+    public void przeliczSkladniki() {
+        String nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, dla którego chcesz przeliczyć składniki:");
+        boolean przepisZnaleziony = false;
+        for (int i = 0; i < przepisy.size(); i++) {
+            if (przepisy.get(i).getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
+                ArrayList<Skladnik> skladniki = przepisy.get(i).getSkladniki();
+                String iloscPorcjiString = JOptionPane.showInputDialog("Podaj ilość porcji, dla której chcesz przeliczyć składniki:");
+                try {
+                    double iloscPorcji = Double.parseDouble(iloscPorcjiString);
+                    for (int j = 0; j < skladniki.size(); j++) {
+                        Skladnik skladnik = skladniki.get(j);
+                        double ilosc = skladnik.getIlosc() * iloscPorcji;
+                        skladnik.setIlosc(ilosc);
                     }
-                    String sposobWykonania = value.getSposobWykonania();
-                    String infoPrzepis = "Nazwa: " + value.getNazwa() + "\nSkladniki:\n" + skladniki + "Sposób wykonania:\n" + sposobWykonania;
-                    JOptionPane.showMessageDialog(null, infoPrzepis);
-                    przepisZnaleziony = true;
-                    break;
+                    przepisy.get(i).setSkladniki(skladniki);
+                    JOptionPane.showMessageDialog(null, "Przeliczono składniki dla przepisu: " + nazwaPrzepisu);
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Nieprawidłowy format ilości porcji");
                 }
-            }
-            if (!przepisZnaleziony) {
-                JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
+                przepisZnaleziony = true;
+                break;
             }
         }
+        if (!przepisZnaleziony) {
+            JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
+        }
+    }
+    public void zapiszPrzepisyDoPliku(String nazwaPliku) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nazwaPliku))) {
+            oos.writeObject(przepisy);
+            System.out.println("Przepisy zostaly zapisane do pliku: " + nazwaPliku);
+        } catch (IOException e) {
+            System.err.println("Blad podczas zapisu przepisow do pliku.");
+            e.printStackTrace();
+        }
+    }
 
-        public void edytujPrzepis(String nazwaPrzepisu) {
-            nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, który chcesz edytować:");
-            boolean przepisZnaleziony = false;
-            for (Przepis przepis : przepisy) {
-                if (przepis.getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
-                    // Similar logic as in the dodajPrzepis() method to edit the recipe
-                    // ...
-                    przepisZnaleziony = true;
-                    break;
-                }
-            }
-            if (!przepisZnaleziony) {
-                JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
-            }
+    public void wczytajPrzepisyZPliku(String nazwaPliku) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nazwaPliku))) {
+            przepisy = (ArrayList<Przepis>) ois.readObject();
+            System.out.println("Przepisy zostaly wczytane z pliku: " + nazwaPliku);
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Blad podczas odczytu przepisow z pliku.");
+            e.printStackTrace();
         }
-
-        public void usunPrzepis(String nazwaPrzepisu) {
-            nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, który chcesz usunąć:");
-            boolean przepisZnaleziony = false;
-            for (int i = 0; i < przepisy.size(); i++) {
-                if (przepisy.get(i).getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
-                    przepisy.remove(i);
-                    JOptionPane.showMessageDialog(null, "Przepis usunięty.");
-                    przepisZnaleziony = true;
-                    break;
-                }
-            }
-            if (!przepisZnaleziony) {
-                JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
-            }
+    }
+    public String[] getNazwyPrzepisow() {
+        String[] nazwyPrzepisow = new String[przepisy.size()];
+        for (int i = 0; i < przepisy.size(); i++) {
+            nazwyPrzepisow[i] = przepisy.get(i).getNazwa();
         }
-
-        public void przeliczSkladniki() {
-            String nazwaPrzepisu = JOptionPane.showInputDialog("Podaj nazwe przepisu, dla którego chcesz przeliczyć składniki:");
-            boolean przepisZnaleziony = false;
-            for (int i = 0; i < przepisy.size(); i++) {
-                if (przepisy.get(i).getNazwa().equalsIgnoreCase(nazwaPrzepisu)) {
-                    ArrayList<Skladnik> skladniki = przepisy.get(i).getSkladniki();
-                    String iloscPorcjiString = JOptionPane.showInputDialog("Podaj ilość porcji, dla której chcesz przeliczyć składniki:");
-                    try {
-                        double iloscPorcji = Double.parseDouble(iloscPorcjiString);
-                        for (int j = 0; j < skladniki.size(); j++) {
-                            Skladnik skladnik = skladniki.get(j);
-                            double ilosc = skladnik.getIlosc() * iloscPorcji;
-                            skladnik.setIlosc(ilosc);
-                        }
-                        przepisy.get(i).setSkladniki(skladniki);
-                        JOptionPane.showMessageDialog(null, "Przeliczono składniki dla przepisu: " + nazwaPrzepisu);
-                    } catch (NumberFormatException e) {
-                        JOptionPane.showMessageDialog(null, "Nieprawidłowy format ilości porcji");
-                    }
-                    przepisZnaleziony = true;
-                    break;
-                }
-            }
-            if (!przepisZnaleziony) {
-                JOptionPane.showMessageDialog(null, "Nie znaleziono przepisu o podanej nazwie.");
-            }
-        }
-        public void zapiszPrzepisyDoPliku(String nazwaPliku) {
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nazwaPliku))) {
-                oos.writeObject(przepisy);
-                System.out.println("Przepisy zostaly zapisane do pliku: " + nazwaPliku);
-            } catch (IOException e) {
-                System.err.println("Blad podczas zapisu przepisow do pliku.");
-                e.printStackTrace();
-            }
-        }
-
-        public void wczytajPrzepisyZPliku(String nazwaPliku) {
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nazwaPliku))) {
-                przepisy = (ArrayList<Przepis>) ois.readObject();
-                System.out.println("Przepisy zostaly wczytane z pliku: " + nazwaPliku);
-            } catch (IOException | ClassNotFoundException e) {
-                System.err.println("Blad podczas odczytu przepisow z pliku.");
-                e.printStackTrace();
-            }
-        }
-            public String[] getNazwyPrzepisow() {
-                String[] nazwyPrzepisow = new String[przepisy.size()];
-                for (int i = 0; i < przepisy.size(); i++) {
-                    nazwyPrzepisow[i] = przepisy.get(i).getNazwa();
-                }
-                return nazwyPrzepisow;
-            }
-        }
+        return nazwyPrzepisow;
+    }
+}
--- Ramka.java
+++ Ramka.java
@@ -1,9 +1,12 @@
 package cookingBook;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Ramka extends JFrame
+public class Ramka extends JFrame implements ActionListener
 {
+    KsiazkaKucharska ksiazkaKucharska = new KsiazkaKucharska();
     JButton bDodajPrzepis;
     JButton bEdytujPrzepis;
     JButton bUsunPrzepis;
@@ -22,38 +25,48 @@ public class Ramka extends JFrame
         bDodajPrzepis = new JButton("Dodaj Przepis");
         bDodajPrzepis.setBounds(10,20,180,20);
         add(bDodajPrzepis);
+        bDodajPrzepis.addActionListener(this);
 
         bEdytujPrzepis = new JButton("Edytuj Przepis");
         bEdytujPrzepis.setBounds(200,20,180,20);
         add(bEdytujPrzepis);
+        bEdytujPrzepis.addActionListener(this);
+
 
         bUsunPrzepis = new JButton("Usuń Przepis");
         bUsunPrzepis.setBounds(390,20,180,20);
         add(bUsunPrzepis);
+        bUsunPrzepis.addActionListener(this);
 
         bWyswietlPrzepis = new JButton("Wyswietl Przepis");
         bWyswietlPrzepis.setBounds(10,50,180,20);
         add(bWyswietlPrzepis);
+        bWyswietlPrzepis.addActionListener(this);
 
         bWyswietlListePrzepisow = new JButton("Wyswietl Listę Przepisów");
         bWyswietlListePrzepisow.setBounds(200,50,180,20);
         add(bWyswietlListePrzepisow);
+        bWyswietlListePrzepisow.addActionListener(this);
 
         bPrzeliczSkladniki = new JButton("Przelicz Składniki");
         bPrzeliczSkladniki.setBounds(10,110,180,20);
         add(bPrzeliczSkladniki);
+        bPrzeliczSkladniki.addActionListener(this);
 
         bWczytajPrzepisy = new JButton("Wczytaj Przepisy");
         bWczytajPrzepisy.setBounds(10,80,180,20);
         add(bWczytajPrzepisy);
+        bWczytajPrzepisy.addActionListener(this);
 
         bZapiszPrzepisy = new JButton("Zapisz Przepisy");
         bZapiszPrzepisy.setBounds(200,80,180,20);
         add(bZapiszPrzepisy);
+        bZapiszPrzepisy.addActionListener(this);
 
         bWyjscie = new JButton("Spadam stąd..");
         bWyjscie.setBounds(390,110,180,20);
         add(bWyjscie);
+        bWyjscie.addActionListener(this);
 
 
 
@@ -68,4 +81,19 @@ public class Ramka extends JFrame
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        Object klawisz = e.getSource();
+        if (klawisz==bDodajPrzepis)
+        {
+            ksiazkaKucharska.dodajPrzepis();
+        } else if (klawisz==bEdytujPrzepis)
+        {
+
+        } else if (klawisz==bWyjscie)
+        {
+            dispose();
+        }
+    }
 }
